066babd71a8501f0e768ead4789eb1a4b9c802dc
Hamza Aoun <aou4925@gmail.com>
2024-04-24 23:26:25 +0300

    adding Admin-dashboard project

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import hamzaAdminDashboard from "./hamzaAdminDashboard.png";
+import hamzaAdminDashboard from "./hamzaAdminDashboard.PNG";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
